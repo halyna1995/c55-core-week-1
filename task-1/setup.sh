49962c3815fa87bc4663ffbf4faf45db3ebe0732
Halyna Romanyshyn <halyna.romanyshyn@gmail.com>
2026-01-15 00:48:53 +0100

    task 1

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,1 +1,50 @@
 # Write your code here
+echo "Creating project..."
+mkdir project
+cd project
+git init
+touch README.md
+mkdir resources
+touch settings.conf
+mkdir src
+cd resources
+touch family picture.jpg
+touch icon.png
+touch logo.png
+cd ..
+cd src
+mkdir database
+mkdir profile
+touch program.java
+cd ..
+git status
+git add .
+git commit -m "Initial commit"
+git push
+sleep 3
+echo "Setup project..."
+echo "Welcome to my project" > README.md
+cd src
+rm -r profile
+cd ..
+cd resources
+rm "family picture.jpg"
+cd ..
+git status
+git add .
+git commit -m "write welcome text to README, remove profile directory , remove family_picture.jpg"
+git push
+sleep 3
+cd resources
+echo "Setup javascript..."
+mv program.java program.js
+echo "console.log('JavaScript works!');" > program.js
+node program.js
+cd ..
+git status
+git add .
+git commit -m "rename program.java to program.js , add log line to program.js"
+git push
+cd ~
+ls -la
+echo "All done!"
