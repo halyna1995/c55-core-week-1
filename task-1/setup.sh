9be109340547792bb92eb4b8ea7c3b9ad935073b
Halyna Romanyshyn <30292604+halyna1995@users.noreply.github.com>
2026-01-19 22:34:30 +0100

    Refactor setup.sh for project initialization
    Refactor setup script to improve organization and commands.

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,4 +1,4 @@
-# Write your code here
+#!/bin/bash
 echo "Creating project..."
 mkdir project
 cd project
@@ -7,44 +7,30 @@ touch README.md
 mkdir resources
 touch settings.conf
 mkdir src
-cd resources
-touch family picture.jpg
-touch icon.png
-touch logo.png
-cd ..
+touch resources/family\ picture.jpg
+touch resources/icon.png
+touch resources/logo.png
 cd src
 mkdir database
 mkdir profile
 touch program.java
 cd ..
-git status
 git add .
-git commit -m "Initial commit"
-git push
+git commit -m "initial commit"
 sleep 3
 echo "Setup project..."
 echo "Welcome to my project" > README.md
-cd src
-rm -r profile
-cd ..
-cd resources
-rm "family picture.jpg"
-cd ..
-git status
+rm -r src/profile
+rm resources/family\ picture.jpg
 git add .
 git commit -m "write welcome text to README, remove profile directory , remove family_picture.jpg"
-git push
+ls -la resources
 sleep 3
-cd resources
 echo "Setup javascript..."
-mv program.java program.js
-echo "console.log('JavaScript works!');" > program.js
-node program.js
-cd ..
-git status
+mv src/program.java src/program.js
+echo "console.log('JavaScript works!');" > src/program.js
+node src/program.js
 git add .
 git commit -m "rename program.java to program.js , add log line to program.js"
-git push
-cd ~
-ls -la
+ls -la ~
 echo "All done!"
